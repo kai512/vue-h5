c65b2df1f4eccfc65d6eeec9aa7b1812bb9ce33c
蒋亮亮 <jliangliang@linewell.com>
2020-05-08 19:46:20 +0800

    组件

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,7 @@ export default {
 	 * @description api请求基础路径
 	 */
 	serviceBaseUrl: {
-		base : process.env.NODE_ENV === 'development' ? '/rest/' : '/rest/'
+		base : process.env.NODE_ENV === 'development' ? '/rest' : '/rest'
 	},
 	/**
 	 * @description 默认打开的首页的路由name值，默认为home
