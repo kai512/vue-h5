3779b66b397ef24ad675ab40ffbff3729c9e8aa6
蒋亮亮 <jliangliang@linewell.com>
2020-08-17 17:53:05 +0800

    bug修改

--- src/components/empty/empty.vue
+++ src/components/empty/empty.vue
@@ -23,5 +23,39 @@ export default {
 };
 </script>
 <style lang="less" scoped>
+    @import '~@/assets/style/color.less';
 
+    //空白页
+    .empty-box{
+        &.bg-white{
+            background-color: white;
+        }
+        background: url(../../assets/images/empty-bg.png)  center 5rem no-repeat;
+        background-size: auto 7.5rem;
+        height: 20rem;
+        box-sizing: border-box;
+        padding-top: 14rem;
+        text-align: center;
+        color: #999;
+        font-size: 0.9375rem;
+        span{
+            display: block;
+        }
+        .btn{
+            font-size: 0.9375rem;
+            color: @main-color;
+            border:solid 1px @main-color;
+            line-height: 2.1875rem;
+            padding: 0 1rem;
+            min-width: 6.25rem;
+            text-align: center;
+            border-radius: 1.125rem;
+            box-sizing: border-box;
+            display: inline-block;
+            vertical-align: top;
+            margin-top: 0.9375rem;
+        }
+    }
+
+    //空白页
 </style>
